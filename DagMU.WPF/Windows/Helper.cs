4dd6fa00f57bb7fb40755c17f3bcae6730ff74e6
drgns <drgn@drgn.org>
2015-04-19 17:21:56 -0400

    Console toggle works.

--- DagMU.WPF/Windows/Helper.cs
+++ DagMU.WPF/Windows/Helper.cs
@@ -10,5 +10,12 @@ namespace DagMUWPF.Windows
 {
 	public abstract class Helper : Window, IHelper
 	{
+		protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+		{
+			base.OnClosing(e);
+
+			e.Cancel = true;
+			this.Hide();
+		}
 	}
 }
