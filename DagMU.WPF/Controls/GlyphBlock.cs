39975965cd12412f281ab30cb7251e16b7d43b1c
dagmu <dagmumuckclient@gmail.com>
2015-11-05 20:55:42 -0500

    GlyphBlock

---
+++ DagMU.WPF/Controls/GlyphBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfTools
+{
+	public class GlyphBlock : TextBlock
+	{
+		public GlyphBlock()
+		{
+			Style = (Style)FindResource("FontAwesome");
+			TextAlignment = TextAlignment.Center;
+			FontSize = 100;
+		}
+	}
+}
